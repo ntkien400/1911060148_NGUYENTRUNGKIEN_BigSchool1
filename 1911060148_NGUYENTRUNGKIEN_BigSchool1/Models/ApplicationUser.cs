10785a7ea5081637e3dd40eee9c01c92ef98e99c
ntkien400 <ntkien400@gmail.com>
2021-06-30 19:07:59 +0700

    Change1

--- 1911060148_NGUYENTRUNGKIEN_BigSchool1/Models/ApplicationUser.cs
+++ 1911060148_NGUYENTRUNGKIEN_BigSchool1/Models/ApplicationUser.cs
@@ -20,6 +20,8 @@ namespace _1911060148_NGUYENTRUNGKIEN_BigSchool1.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
