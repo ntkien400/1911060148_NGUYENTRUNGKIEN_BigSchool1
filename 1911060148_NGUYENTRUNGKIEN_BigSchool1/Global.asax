f87ec9e744f960182c0edef9d346c6a8f1ca328c
ntkien400 <ntkien400@gmail.com>
2021-06-30 19:03:23 +0700

    Add project files.

---
+++ 1911060148_NGUYENTRUNGKIEN_BigSchool1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1911060148_NGUYENTRUNGKIEN_BigSchool1.MvcApplication" Language="C#" %>
---
+++ 1911060148_NGUYENTRUNGKIEN_BigSchool1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1911060148_NGUYENTRUNGKIEN_BigSchool1.Startup))]
+namespace _1911060148_NGUYENTRUNGKIEN_BigSchool1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
